15100d1cfb698364496246dc1740e22b257425a8
Alex <spider_inter@mail.ru>
2023-03-26 20:37:06 +0300

    Выполнил 4 задания второго урока

--- Program.cs
+++ Program.cs
@@ -1,44 +1,77 @@
-﻿firstNumber();
-secondNumber();
-days();
+﻿ maxMin();
+ maxMinTh();
+ evenNumber();
+ evenNumberArray();
 
+int maxMin(){
+    Console.Write("Первое задание---> Введите первое число: ");
+    int max = 0;
+    int min = 0;
+     int number = Convert.ToInt32(Console.ReadLine());
+      Console.Write("Первое задание---> Введите второе число: ");
+     int number1 = Convert.ToInt32(Console.ReadLine());
+    
+    if (number == number1){
+        Console.WriteLine("Числа эквивалентны");
+    }
+     if (number < number1){
+ max= number1;
+ min = number;
+        Console.WriteLine($"Максимальное число {max} минимальное {min}");
+    }if (number > number1){
+ max = number;
+ min = number1;
+Console.WriteLine($"Максимальное число {max} минимальное {min}");
+    }
+return  0;
 
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-int firstNumber(){
-  Console.Write("Первое задание---> Введите трехнзачное число : ");
-  int number = Convert.ToInt32(Console.ReadLine());
- 
- if (number >=100 && number <1000){
-
-    int res = (number % 100 - number %10)/10;
-    System.Console.WriteLine($"Вторая цифра числа {number} является {res}");
 }
-else 
-System.Console.WriteLine($"Число {number} не является трехзначным числом");
-return number;
-}
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-int secondNumber(){
 
-Console.Write("Второе задание---> Введите трехзначную цифру :");
-int number = Convert.ToInt32(Console.ReadLine());
-int res = number%10;
-int num = 100;
-  if (number >= num){
-  System.Console.WriteLine($"Третьим числом является: {res}");
-  }else
-  System.Console.WriteLine($"Не являеться трехзначным числом");
+int maxMinTh(){
+ int max = 1;
+   Console.Write("Второе задание---> Введите первое число: ");
+   int a = Convert.ToInt32(Console.ReadLine());
+   Console.Write("Второе задание---> Введите второе число: ");
+   int b = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Второе задание---> Введите третье число: ");
+   int r = Convert.ToInt32(Console.ReadLine());
+   if(max<a){
+     max = a;
+    }
 
-return res;
+     if(max<b){
+     max = b;
+    }
+
+     if(max<r){
+     max = r;
+    }
+
+  Console.WriteLine($"Максимальное число {max}");
+    return 0;
 }
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
- int days(){
-    Console.Write("Третье задание ---> Введите день :");
+
+
+int evenNumber (){
+    Console.WriteLine($"Третье задание---> Введите число: ");
    int number = Convert.ToInt32(Console.ReadLine());
-   if (number>=1&&number<=5){
-       System.Console.WriteLine($"Нет");
-   }else
-   System.Console.WriteLine($"Да");
-   return number;
- }
+   if (number % 2==0)
+            {
+                Console.WriteLine("число четное");
+            }else
+            Console.WriteLine("число не четное");
+                return 0;
+}
+
+int evenNumberArray(){
+    Console.Write("Четвертое задание---> Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+   
+    for (int i = 1; i < number; i++)
+{
+     if (i % 2==0){
+        Console.Write(i+" ");
+     }
+}
+return 0;
+}
